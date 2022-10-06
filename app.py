9f92f2a29f7d48f54c2398079adc1ab6c697f59d
SpookyDlaw <torressolje@hotmail.com>
2022-10-06 15:37:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-import time
-
-import redis
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+def home():
+    return render_template('registry.html')
+
+if __name__ == '__main__':
+    # Run the app server on localhost:4449
+    app.run(host='0.0.0.0',port=4449)
+
 
